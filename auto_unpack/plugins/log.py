413a3b6546d96f2ab2cfc42f740f86072ae7d197
xiaohuohumax <1915970986@qq.com>
2024-08-12 00:57:59 +0800

    log插件增加显示文件信息功能

--- auto_unpack/plugins/log.py
+++ auto_unpack/plugins/log.py
@@ -1,9 +1,11 @@
 import logging
-from typing import Literal
+from datetime import datetime
+from typing import Any, Callable, Dict, List, Literal, Optional
 
-from pydantic import Field
+from pydantic import BaseModel, Field
 
 from auto_unpack.plugin import OutputPluginConfig, Plugin
+from auto_unpack.store import Context, FileData
 from auto_unpack.util.file import get_next_not_exist_path, write_file
 
 logger = logging.getLogger(__name__)
@@ -21,6 +23,44 @@ class LogPluginConfig(OutputPluginConfig):
         default='log',
         description="日志文件名(默认: log)"
     )
+    print_stats: List[Literal['all', 'ctime', 'mtime']] = Field(
+        default=[],
+        description="打印文件信息(默认: [])\n'all'：所有信息\n'ctime'：创建时间\n'mtime'：修改时间"
+    )
+
+
+class LogFileStat(BaseModel):
+    """
+    文件信息
+    """
+    ctime: str = Field(
+        default=None,
+        description="创建时间"
+    )
+    mtime: str = Field(
+        default=None,
+        description="修改时间"
+    )
+
+
+class LogFileData(FileData):
+    """
+    文件数据
+    """
+    stat: Optional[LogFileStat] = Field(
+        default=None,
+        description="文件信息"
+    )
+
+
+class LogContext(Context):
+    """
+    日志类
+    """
+    file_datas: List[LogFileData] = Field(
+        default=[],
+        description="文件数据"
+    )
 
 
 class LogPlugin(Plugin[LogPluginConfig]):
@@ -31,6 +71,56 @@ class LogPlugin(Plugin[LogPluginConfig]):
     """
     name: str = "log"
 
+    def _timestamp_to_str(self, timestamp: float) -> str:
+        """
+        时间戳转字符串
+        """
+        return datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+
+    def _file_data_to_log_file_data(self, file_data: FileData) -> LogFileData:
+        """
+        转换文件数据为日志文件数据
+        """
+        file_info = LogFileStat()
+
+        stat_map: Dict[str, Callable[[FileData], Any]] = {
+            'ctime': lambda x: self._timestamp_to_str(x.path.stat().st_ctime),
+            'mtime': lambda x: self._timestamp_to_str(x.path.stat().st_mtime),
+        }
+
+        if 'all' in self.config.print_stats:
+            for stat in stat_map.keys():
+                file_info = file_info.model_copy(update={
+                    stat: stat_map[stat](file_data)
+                })
+        else:
+            for stat in self.config.print_stats:
+                if stat in stat_map.keys():
+                    file_info = file_info.model_copy(update={
+                        stat: stat_map[stat](file_data)
+                    })
+
+        return LogFileData(
+            path=file_data.path,
+            search_path=file_data.search_path,
+            stat=file_info
+        )
+
+    def _context_to_log_context(self, context: Context) -> LogContext:
+        """
+        转换上下文信息为文件数据
+        """
+        if len(self.config.print_stats) == 0:
+            return LogContext(file_datas=[
+                LogFileData(path=f.path, search_path=f.search_path, stat=None)
+                for f in context.file_datas
+            ])
+
+        return LogContext(file_datas=[
+            self._file_data_to_log_file_data(f)
+            for f in context.file_datas
+        ])
+
     def execute(self):
         context = self.load_context()
 
@@ -38,6 +128,10 @@ class LogPlugin(Plugin[LogPluginConfig]):
             self.global_config.info_dir / f'{self.config.file_name}.json'
         )
 
-        write_file(log_path, context.model_dump_json(indent=2))
+        log_context = self._context_to_log_context(context)
+
+        write_file(log_path, log_context.model_dump_json(
+            indent=2, exclude_none=True
+        ))
 
         logger.info(f'Log file data saved to {log_path}')
--- docs/page/plugins/builtin/log.md
+++ docs/page/plugins/builtin/log.md
@@ -22,15 +22,16 @@ icon: material/clipboard-edit-outline
 
     `auto_unpack.plugins.log.LogPluginConfig`
 
-| 名称                      | 类型           | 描述                     | 默认值  |
-| ------------------------- | -------------- | ------------------------ | ------- |
-| :star: `name`             | Literal['log'] | 插件名称，固定为 `'log'` | `'log'` |
-| `file_name`               | str            | 日志文件名               | `'log'` |
-| [`上下文字段见上文`](#_1) |                |                          |         |
+| 名称                      | 类型                                   | 描述                                                                               | 默认值  |
+| ------------------------- | -------------------------------------- | ---------------------------------------------------------------------------------- | ------- |
+| :star: `name`             | Literal['log']                         | 插件名称，固定为 `'log'`                                                           | `'log'` |
+| `file_name`               | str                                    | 日志文件名                                                                         | `'log'` |
+| `print_stats`             | List[Literal['all', 'ctime', 'mtime']] | 打印文件信息的字段<br/>`all`：所有信息<br/>`ctime`：创建时间<br/>`mtime`：修改时间 | `[]`    |
+| [`上下文字段见上文`](#_1) |                                        |                                                                                    |         |
 
 ## :recycle: 示例
 
-### 打印文件夹 archive 下所有文件
+### 打印文件夹 archive 下所有文件，并且打印创建时间和修改时间
 
 ```yaml
 flow:
@@ -39,4 +40,7 @@ flow:
       dir: archive
     - name: log
       file_name: log
+      print_stats:
+        - ctime
+        - mtime
 ```
